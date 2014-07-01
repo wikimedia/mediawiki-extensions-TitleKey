3fa071a1e690920ae64b22d49f4d40c11ddc6131
Antoine Musso <hashar@free.fr>
2014-07-01 15:19:01 +0200

    Sqlite compatibility
    Bug: 65896
    Change-Id: I98366c3e8ff8615bb7462c594ca1a8b97ba5a42b

--- titlekey.sql
+++ titlekey.sql
@@ -1,15 +1,14 @@
 CREATE TABLE /*$wgDBprefix*/titlekey (
   -- Ref to page_id
-  tk_page int unsigned NOT NULL,
+  tk_page int unsigned NOT NULL PRIMARY KEY,
 
   -- Keep a denormalized copy of the namespace for filtering
   tk_namespace int NOT NULL,
 
   -- Normalized title.
   -- With namespace prefix, case-folded, in space form.
-  tk_key varchar(255) binary NOT NULL,
-  
-  PRIMARY KEY tk_page (tk_page),
-  INDEX name_key (tk_namespace, tk_key)
+  tk_key varchar(255) binary NOT NULL
 
 ) /*$wgDBTableOptions*/;
+
+CREATE INDEX /*i*/name_key ON /*$wgDBprefix*/titlekey (tk_namespace, tk_key);
